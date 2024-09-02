7f0e7320760109265642460547da3d2591a1f73f
Cheid66 <cheid66@gmail.com>
2024-09-02 23:10:35 +0500

    Add files via upload

--- Переменные практика.py
+++ Переменные практика.py
@@ -1,5 +1,5 @@
-number_of_tasks = '12'
-amount_of_time = '1.5'
+number_of_tasks = 12
+amount_of_time = 1.5
 cource_name = 'Python'
-average_time = '0.125'
-print('Курс: ' + cource_name + ', всего задач: ' + number_of_tasks + ', затрачено часов: ' + amount_of_time + ', среднее время выполнения: ' + average_time + ' часа')
+average_time = amount_of_time/number_of_tasks
+print('Курс: ', cource_name, ', всего задач: ', number_of_tasks, ', затрачено часов: ', amount_of_time, ', среднее время выполнения: ', average_time, ' часа')
